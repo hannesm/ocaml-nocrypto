40367b6389b5b2eebb28f40ea43fd0eedb7ae415
Hannes Mehnert <hannes@mehnert.org>
2019-07-01 22:53:44 +0200

    in order

--- src/native/endian.h
+++ src/native/endian.h
@@ -28,12 +28,6 @@
 
 /* For freestanding builds */
 #define	__bswap16_gen(x)	(uint16_t)((x) << 8 | (x) >> 8)
-#define	__bswap32_gen(x)		\
-	(((uint32_t)__bswap16((x) & 0xffff) << 16) | __bswap16((x) >> 16))
-#define	__bswap64_gen(x)		\
-	(((uint64_t)__bswap32((x) & 0xffffffff) << 32) | __bswap32((x) >> 32))
-
-/* These are defined as functions to avoid multiple evaluation of x. */
 
 static __inline uint16_t
 __bswap16_var(uint16_t _x)
@@ -42,12 +36,26 @@ __bswap16_var(uint16_t _x)
         return (__bswap16_gen(_x));
 }
 
+#define	__bswap16(x)				\
+	((uint16_t)(__builtin_constant_p(x) ?	\
+	    __bswap16_gen((uint16_t)(x)) : __bswap16_var(x)))
+
+#define	__bswap32_gen(x)                                                \
+	(((uint32_t)__bswap16((x) & 0xffff) << 16) | __bswap16((x) >> 16))
+
 static __inline uint32_t
 __bswap32_var(uint32_t _x)
 {
 
           return (__bswap32_gen(_x));
 }
+
+#define	__bswap32(x)			\
+	(__builtin_constant_p(x) ?	\
+         __bswap32_gen((uint32_t)(x)) : __bswap32_var(x))
+
+#define	__bswap64_gen(x)                                                \
+	(((uint64_t)__bswap32((x) & 0xffffffff) << 32) | __bswap32((x) >> 32))
 
 static __inline uint64_t
 __bswap64_var(uint64_t _x)
@@ -56,16 +64,12 @@ __bswap64_var(uint64_t _x)
         return (__bswap64_gen(_x));
 }
 
-#define	__bswap16(x)				\
-	((uint16_t)(__builtin_constant_p(x) ?	\
-	    __bswap16_gen((uint16_t)(x)) : __bswap16_var(x)))
-#define	__bswap32(x)			\
-	(__builtin_constant_p(x) ?	\
-         __bswap32_gen((uint32_t)(x)) : __bswap32_var(x))
 #define	__bswap64(x)			\
 	(__builtin_constant_p(x) ?	\
 	    __bswap64_gen((uint64_t)(x)) : __bswap64_var(x))
+
 #define bswap64(x) __bswap64(x)
+
 #define htobe64(x) bswap64((x))
 #define be64toh(x) bswap64((x))
 
